209f19c52510b7c588ce9fdda001bd292c90d698
Erv16 <epalani1@binghamton.edu>
2020-06-13 18:30:58 -0400

    Added functionality for the Vehicle Service to save vehicles and retrieve them

--- src/main/java/io/egen/service/VehicleServiceImpl.java
+++ src/main/java/io/egen/service/VehicleServiceImpl.java
@@ -1,6 +1,8 @@
 package io.egen.service;
 
 import io.egen.entity.Vehicle;
+import io.egen.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,15 +11,20 @@ import java.util.List;
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
     @Override
     @Transactional
     public List<Vehicle> saveVehicle(List<Vehicle> vehicles) {
-        return null;
+        vehicles.forEach(vehicle -> vehicleRepository.save(vehicle));
+        return vehicles;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Vehicle> getVehicles() {
-        return null;
+
+        return (List<Vehicle>) vehicleRepository.findAll();
     }
 }
