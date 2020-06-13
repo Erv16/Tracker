ae0c3086ce6a52c795c5fd17c92f97047a40736b
Erv16 <epalani1@binghamton.edu>
2020-06-13 16:50:47 -0400

    Refactored the Application file to a Spring Boot Application file and created an Entity for Vehicle

---
+++ src/main/java/io/egen/entity/Vehicle.java
@@ -0,0 +1,93 @@
+package io.egen.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    private String vin;
+
+    private String make;
+
+    private String model;
+
+    private String year;
+
+    private Double redLineRpm;
+
+    private Double maxFuelVolume;
+
+    private Timestamp lastServiceDate;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Double getRedLineRpm() {
+        return redLineRpm;
+    }
+
+    public void setRedLineRpm(Double redLineRpm) {
+        this.redLineRpm = redLineRpm;
+    }
+
+    public Double getMaxFuelVolume() {
+        return maxFuelVolume;
+    }
+
+    public void setMaxFuelVolume(Double maxFuelVolume) {
+        this.maxFuelVolume = maxFuelVolume;
+    }
+
+    public Timestamp getLastServiceDate() {
+        return lastServiceDate;
+    }
+
+    public void setLastServiceDate(Timestamp lastServiceDate) {
+        this.lastServiceDate = lastServiceDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vin='" + vin + '\'' +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year='" + year + '\'' +
+                ", redLineRpm=" + redLineRpm +
+                ", maxFuelVolume=" + maxFuelVolume +
+                ", lastServiceDate=" + lastServiceDate +
+                '}';
+    }
+}
